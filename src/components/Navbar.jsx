01d0ecec7f808a40d911690bb4582c8cf7aa9232
dpvasani <vadanidarshan@gmail.com>
2024-01-11 00:02:53 +0530

    first commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,7 +8,6 @@ import logo from '../assets/img/logo.jpg';
 import { useLocation } from "react-router-dom";
 import { useEffect } from "react";
 import { Link as ScrollLink, animateScroll as scroll } from 'react-scroll';
-
 const Navbar = () => {
   const [menu, setMenu] = useState(false);
 
@@ -20,10 +19,9 @@ const Navbar = () => {
     console.log('Vraj');
     setMenu(false);
   };
-
   const location = useLocation();
-
   const handleHomeLink = () => {
+    console.log(location.pathname);
     if (location.pathname === '/') {
       return (
         <ScrollLink
@@ -48,9 +46,10 @@ const Navbar = () => {
       );
     }
   };
-
   const handleAboutLink = () => {
+    console.log(location.pathname);
     if (location.pathname === '/') {
+      // If yes, scroll to the 'home' section within the Dashboard
       return (
         <ScrollLink
           className='hover:text-brightColor transition-all cursor-pointer'
@@ -74,9 +73,10 @@ const Navbar = () => {
       );
     }
   };
-
   const handleTermsLink = () => {
+    console.log(location.pathname);
     if (location.pathname === '/') {
+      // If yes, scroll to the 'home' section within the Dashboard
       return (
         <ScrollLink
           className='hover:text-brightColor transition-all cursor-pointer'
@@ -100,9 +100,10 @@ const Navbar = () => {
       );
     }
   };
-
   const handleDishesLink = () => {
+    console.log(location.pathname);
     if (location.pathname === '/') {
+      // If yes, scroll to the 'home' section within the Dashboard
       return (
         <ScrollLink
           className='hover:text-brightColor transition-all cursor-pointer'
@@ -126,24 +127,20 @@ const Navbar = () => {
       );
     }
   };
-
   useEffect(() => {
+    // Your code here
     handleAboutLink();
     handleDishesLink();
     handleHomeLink();
     handleTermsLink();
   }, [location.pathname]);
-
-  const handleBuyClick = () => {
-    window.open("https://wa.me/message/SISUP46GKRC6H1", "_blank");
-  };
-
   return (
     <div className='fixed w-full'>
       <div>
         <div className=' flex flex-row justify-between p-5 md:px-32 lg:px-5 md:px-2 bg-white shadow-[0_3px_10px_rgb(0,0,0,0.2)]'>
           <div className=' flex flex-row items-center cursor-pointer'>
             <span>
+              {/* Replace the BiRestaurant with your logo */}
               <img src={logo} alt='Logo' className='h-9 w-29' />
             </span>
             <h1 className=' text-xl ml-4 font-semibold'>Cluster Globals</h1>
@@ -167,14 +164,71 @@ const Navbar = () => {
               </div>
 
               <ul className='absolute hidden group-hover:block bg-white border border-gray-300 w-[252px] rounded-lg p-5 '>
-                {/* ... (rest of the code) */}
+                <li>
+                  <scroll
+                    to='dishes'
+                    spy={true}
+                    smooth={true}
+                    duration={500}
+                    className='text-gray-800 hover:text-brightColor transition-all cursor-pointer'
+                  >
+                    <Link to={'/Fresh Vegetables'}> Fresh Vegetables</Link>
+                  </scroll>
+                </li>
+                <hr class='w-42 ml-1 h-[0.1rem] mx-auto my-3 bg-gray-200 border-0 rounded dark:bg-gray-700'></hr>
+                <li>
+                  <scroll
+                    to='dishes'
+                    spy={true}
+                    smooth={true}
+                    duration={500}
+                    className='text-gray-800 hover:text-brightColor transition-all cursor-pointer'
+                  >
+                    <Link to={'/Fresh Fruits'}>Fresh Fruits</Link>
+                  </scroll>
+                </li>
+                <hr class='w-42 ml-1 h-[0.1rem] mx-auto my-3 bg-gray-200 border-0 rounded dark:bg-gray-700'></hr>
+                <li>
+                  <scroll
+                    to='dishes'
+                    spy={true}
+                    smooth={true}
+                    duration={500}
+                    className='text-gray-800 hover:text-brightColor transition-all cursor-pointer'
+                  >
+                    <Link to={'/Kitchen Spices & Puries'}> Kitchen Spices & Puries</Link>
+                  </scroll>
+                </li>
+                <hr class='w-42 ml-1 h-[0.1rem] mx-auto my-3 bg-gray-200 border-0 rounded dark:bg-gray-700'></hr>
+                <li>
+                  <scroll
+                    to='dishes'
+                    spy={true}
+                    smooth={true}
+                    duration={500}
+                    className='text-gray-800 hover:text-brightColor transition-all cursor-pointer'
+                  >
+                    <Link to={'/Dehydrated Flakes & Powder'}> Dehydrated Flakes & Powder</Link>
+                  </scroll>
+                </li>
+                <hr class='w-42 ml-1 h-[0.1rem] mx-auto my-3 bg-gray-200 border-0 rounded dark:bg-gray-700'></hr>
+                <li>
+                  <scroll
+                    to='dishes'
+                    spy={true}
+                    smooth={true}
+                    duration={500}
+                    className='hover:text-brightColor transition-all cursor-pointer'
+                  >
+                    <Link to={'/Eco Packaging'}> Eco Packaging</Link>
+                  </scroll>
+                </li>
               </ul>
             </div>
-
             <div>{handleAboutLink()}</div>
             <div>{handleTermsLink()}</div>
 
-            <Button title='Buy' onClick={handleBuyClick} />
+            <Button title='Buy' />
           </nav>
 
           <div className='md:hidden flex items-center'>
@@ -185,13 +239,46 @@ const Navbar = () => {
             )}
           </div>
         </div>
-
         <div
           className={` ${
             menu ? 'translate-x-0' : '-translate-x-full'
           } lg:hidden flex flex-col z-2 h-screen absolute bg-black text-white left-0 top-20 font-semibold text-2xl text-center pt-8 pb-4 gap-8 w-full h-fit transition-transform duration-300`}
         >
-          {/* ... (rest of the code) */}
+          <div >{handleHomeLink()}</div>
+          <div onClick={closeMenu}>{handleAboutLink()}</div>
+          <div onClick={closeMenu}>{handleTermsLink()}</div>
+          <div onClick={closeMenu}>{handleDishesLink()}</div>
+          {/* <scroll
+            to='dishes'
+            spy={true}
+            smooth={true}
+            duration={500}
+            className='hover:text-brightColor transition-all cursor-pointer'
+          >
+            <Link to={'/Eco Packaging'}> Eco Packaging</Link>
+          </scroll> */}
+          {/*
+          <scroll
+            to='review'
+            spy={true}
+            smooth={true}
+            duration={500}
+            className=' hover:text-brightColor transition-all cursor-pointer'
+            onClick={closeMenu}
+          >
+            Certifications
+          </scroll>
+          <scroll
+            to='review'
+            spy={true}
+            smooth={true}
+            duration={500}
+            className=' hover:text-brightColor transition-all cursor-pointer'
+            onClick={closeMenu}
+          >
+            Blogs
+          </scroll> */}
+
           <Button title='Login' />
         </div>
       </div>
